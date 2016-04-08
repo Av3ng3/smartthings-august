75f85864dbfa694db870320877c2510461437532
Andy Savage <andy@savage.hk>
2016-04-08 13:39:02 +0800

    Changed API to use August Connect

--- devicehandlers/augustlock.groovy
+++ devicehandlers/augustlock.groovy
@@ -20,8 +20,15 @@
  */
 
 preferences {
-    input("serverAddress", "text", title: "Server", description: "Your August-HTTP Server IP", required: "true", defaultValue: "test.wowwee.hk", displayDuringSetup: "true")
-    input("serverPort", "number", title: "Port", description: "Your August-HTTP Server Port", required: "true", defaultValue: 80, displayDuringSetup: "true")
+    //input("serverAddress", "text", title: "Server", description: "Your August-HTTP Server IP", required: "true", defaultValue: "test.wowwee.hk", displayDuringSetup: "true")
+    ///input("serverPort", "number", title: "Port", description: "Your August-HTTP Server Port", required: "true", defaultValue: 80, displayDuringSetup: "true")
+    input("augustUsername", "text", title: "August Username", description: "Your August Username for login, this is either email:you@blah.com or phone:+XXXXXXXXXX", required: "true", defaultValue: "", displayDuringSetup: "true")
+    input("augustPassword", "text", title: "August Password", description: "Your August Password for login", required: "true", defaultValue: "", displayDuringSetup: "true")
+
+    input("lockId", "text", title: "Lock Identifier", description: "Your August Lock Identifier", required: "true", defaultValue: "000", displayDuringSetup: "true")
+    input("apiKey", "text", title: "API Key", description: "Your August Connect API Key", required: "true", defaultValue: "14445b6a2dba", displayDuringSetup: "true")
+    input("autoLockSecs", "number", title: "Auto Lock Secs", description: "Seconds that your lock is set to auto-lock (0 for disabled)", required: "true", defaultValue: "", displayDuringSetup: "true")
+    input("apiBaseUrl", "text", title: "API Base URL", description: "Where to send API requests to (only change this for testing)", required: "true", defaultValue: "https://api-production.august.com", displayDuringSetup: "true")
 }
 
 metadata {
@@ -33,10 +40,10 @@ metadata {
         capability "Refresh"
         //capability "Battery"
 
-        attribute "lockStatus", "enum", ["locking", "locked", "unlocking", "unlocked", "unknown"]
-        attribute "autoLock", "enum", ["true", "false"]
-        attribute "secondsUntilAutoLock", "number"
-        attribute "autoLockTime", "number"
+        //attribute "lockStatus", "enum", ["locking", "locked", "unlocking", "unlocked", "unknown"]
+        //attribute "autoLock", "enum", ["true", "false"]
+        //attribute "secondsUntilAutoLock", "number"
+        //attribute "autoLockTime", "number"
         //attribute "remainingAutoLockTime", "number"
 
         //command "getStatus", ["boolean"]
@@ -46,11 +53,11 @@ metadata {
         //command "unlockAndEnableAutoLock" ["number", "relockTime"]
     }
 
-    simulator {
-        //reply "cached": "{'remaining':'3600', 'lock':'locked', 'everlocktime':'3600'}"
-
-        reply "reamining":"3600","lock":"locked","everlocktime":"3600"
-    }
+    // simulator {
+    //     //reply "cached": "{'remaining':'3600', 'lock':'locked', 'everlocktime':'3600'}"
+    //
+    //     reply "reamining":"3600","lock":"locked","everlocktime":"3600"
+    // }
 
     tiles {
         standardTile("toggle", "device.lock", width: 2, height: 2) {
@@ -75,122 +82,156 @@ metadata {
         standardTile("refresh", "device.lock", inactiveLabel: false, decoration: "flat") {
                 state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
         }
-        standardTile("configure", "device.configure", inactiveLabel: false, decoration: "flat") {
-                state "configure", label:'', action:"configuration.configure", icon:"st.secondary.configure"
-        }
+        // standardTile("configure", "device.configure", inactiveLabel: false, decoration: "flat") {
+        //         state "configure", label:'', action:"configuration.configure", icon:"st.secondary.configure"
+        // }
         main "toggle"
-        details(["toggle", "lock", "unlock", "configure", "refresh"])
+        details(["toggle", "lock", "unlock", "refresh"])
     }
 }
 
-def parseEventData(Map results) {
-    results.each { name, value ->
-        if (name == "remaining") {
-            sendEvent(name: 'secondsUntilAutoLock', value: value)
-        } else if (name == "lock") {
-            if (value == "locked") {
-                sendEvent(name: 'lockStatus', value: "locked")
-            } else if (value == "unlocked") {
-                sendEvent(name: 'lockStatus', value: "unlocked")
+def userAgent = "August/4.4.42 (iPhone; iOS 9.0.2; Scale/2.00)";
+
+def getAccessToken(username, password, installId) {
+    def postParams = [
+        uri: $apiBaseUrl,
+        path: "/session",
+        headers: [
+            "x-kease-api-key": "$apiKey",
+            "Accept-Encoding": "gzip, deflate",
+            "Proxy-Connection": "keep-alive",
+            "Connection": "keep-alive",
+            "User-Agent": "$userAgent",
+            "Accept-Language": "en-US;q=1",
+            "accept-version": "0.0.1"
+        ],
+        body: [
+            "identifier": $username,
+            "installId": $installId || "000",
+			"password": $password
+        ]
+    ]
+
+    try {
+        httpPostJson(postParams) { response ->
+            if (response.status < 299) {
+                def accessToken = reasponse.headers["x-august-access-token"]
+                log.trace "Got Access Token: $accessToken"
+                state.apiAccessToken = accessToken
+            } else {
+                log.trace "Got a strange response from server: $response"
             }
-        } else if (name == "everlocktime") {
-            sendEvent(name: 'autoLockTime', value: value)
         }
+    } catch (e) {
+        log.trace "Failed to get Access Token $e"
     }
 }
 
 def lock() {
     log.debug "Executing 'lock'"
-    api('lock', null)
-    sendEvent(name: 'lockStatus', value: "locking")
+    api('lock_door')
 }
 
 def unlock() {
     log.debug "Executing 'unlock'"
-    api('unlock')
-    sendEvent(name: 'lockStatus', value: "unlocking")
+    api('unlock_door')
 }
 
 def poll() {
     log.debug "Executing 'poll'"
-    results = parent.pollChildren()
-    parseEventData(results)
+    api('cached_status')
 }
 
 def refresh() {
     log.debug "Executing 'refresh'"
-    poll()
-    //getStatus(false)
+    api('real_status')
 }
 
 def configure() {
     log.debug "Executing 'configure'"
 }
 
-/** Custom Commands **/
-def getStatus(Boolean cached = "true") {
-    if (cached == "true") {
-        api("cached")
-    } else {
-        api("status")
+def setToLocked() {
+	sendEvent(name: "lock", value: "locked")
+	log.trace "Changed back to 'locked' since $autoLockSecs seconds have passed."
+	//sendNotificationEvent("Front Door Lock was automatically re-locked after 30 seconds.")
+}
+
+private api(String command) {
+    if (!state.apiAccessToken) {
+        getAccessToken($augustUsername, $augustPassword, "000")
     }
-}
 
-// def getRelockTime() {
-//     api("relocktime")
-// }
+    return
 
-private api(String command, Map params = null) {
-    def result = new physicalgraph.device.HubAction(
-        method: "GET",
-        path: "/august/control/${command}",
+    def path = "";
+    if (command == 'real_status') {
+        path = "/remoteoperate/$lockId/status"
+    } else if (command == 'cached_status') {
+        path = "/locks/$lockId"
+    } else if (command == 'lock_door') {
+        sendEvent(name: 'lock', value: "locking")
+        path = "/remoteoperate/$lockId/lock"
+    } else if (command == 'unlock_door') {
+        sendEvent(name: 'lock', value: "unlocking")
+        path = "/remoteoperate/$lockId/unlock"
+    } else {
+        log.debug "Invalid Command $command"
+    }
+
+    def connectAPIHeaders = [
+        uri: $apiBaseUrl,
+        path: path,
         headers: [
-            HOST: getHostAddress()
-        ],
-        query: params
-    )
-}
+        "x-kease-api-key": "$apiKey",
+        "x-august-access-token": "$apiAccessToken",
+        "Accept-Encoding": "gzip, deflate",
+        "Proxy-Connection": "keep-alive",
+        "Connection": "keep-alive",
+        "User-Agent": "$userAgent",
+        "Accept-Language": "en-US;q=1",
+        "accept-version": "0.0.1"
+        ]
+    ]
 
-def parse(description) {
-    def msg = parseLanMessage(description)
-
-    def headersAsString = msg.header // => headers as a string
-    def headerMap = msg.headers      // => headers as a Map
-    def body = msg.body              // => request body as a string
-    def status = msg.status          // => http status code of the response
-    def json = msg.json              // => any JSON included in response body, as a data structure of lists and maps
-    def xml = msg.xml                // => any XML included in response body, as a document tree structure
-    def data = msg.data              // => either JSON or XML in response body (whichever is specified by content-type header in response)
-}
-
-// gets the address of the hub
-private getCallBackAddress() {
-    return device.hub.getDataValue("localIP") + ":" + device.hub.getDataValue("localSrvPortTCP")
-}
-
-// gets the address of the device
-private getHostAddress() {
-    def ip = getDataValue("ip")
-    def port = getDataValue("port")
-
-    if (!ip || !port) {
-        def parts = device.deviceNetworkId.split(":")
-        if (parts.length == 2) {
-            ip = parts[0]
-            port = parts[1]
+    try {
+        httpPutJson(connectAPIHeaders) { response ->
+             parse(response);
+         }
+    } catch (e) {
+        log.error "Something went wrong while executing command: $e"
+        if (command == 'unlock') {
+            log.trace "Setting state to unlocked anyway" //Again, I set the lock to 'unlocked' even though it failed because the lock seems to always still do what I requested, even if ST times out.
+            sendEvent(name: "lock", value: "unlocked")
+            runIn($autoLockSecs, setToLocked)
         } else {
-            log.warn "Can't figure out ip and port for device: ${device.id}"
+            // Assuming door is locked due to auto-lock
+            sendEvent(name: "lock", value: "locked")
         }
     }
-
-    log.debug "Using IP: $ip and port: $port for device: ${device.id}"
-    return convertHexToIP(ip) + ":" + convertHexToInt(port)
 }
 
-private Integer convertHexToInt(hex) {
-    return Integer.parseInt(hex,16)
-}
-
-private String convertHexToIP(hex) {
-    return [convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
+def parse(response) {
+    if (response.status != 200 ) {
+        log.trace "Failed test"
+        log.trace "There was an error executing $command"
+        //sendNotificationEvent("There was an error unlocking Front Door Lock.")
+    } else if (response.status == 200) {
+       def answer = response.data
+       def realAnswer = answer.status
+       log.trace "${answer.status}"
+       if (realAnswer == "kAugLockState_Unlocked") {
+           sendEvent(name: "lock", value: "unlocked")
+           //sendNotificationEvent("I have confirmed that Front Door Lock was unlocked.")
+           // If we have just executed the command to unlock the door, set the door autolock
+           if ($autoLockSecs > 0 && command == 'unlock_door') {
+               runIn($autoLockSecs, setToLocked)
+           }
+        } else if (realAnswer == "kAugLockState_Locked") {
+            sendEvent(name: "lock", value: "locked")
+        } else {
+            log.trace "There was an error unlocking the Front Door Lock."
+            //sendNotificationEvent("There was an error unlocking Front Door Lock.")
+        }
+    }
 }
